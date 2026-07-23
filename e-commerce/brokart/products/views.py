567cf80a89c074fdf56c0ce428b451a295ce793e
mihadmac <macbornio@icloud.com>
2026-07-23 13:18:24 +0530

    Add product list pagination

--- e-commerce/brokart/products/views.py
+++ e-commerce/brokart/products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Product
-
+from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -8,11 +8,14 @@ def index(request):
 
 
 def list_product(request):
+    page=1
+    if request.method=='GET':
+        page=request.GET.get('page',1)
     product_list=Product.objects.all()
-    context={
-        'products': product_list
-    }
-    return render(request, 'products.html', context)
+    products_paginator = Paginator(product_list, 3)  # Show 4 products per page
+    products_page_number = products_paginator.get_page(page)
+
+    return render(request, 'products.html', {'products': products_page_number})
 
 
 
